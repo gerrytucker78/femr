e073e5bcf82e51090cdce838c378c3523fb571d7
Gerry Tucker <gerrytucker@gmail.com>
2016-11-12 11:01:50 -0600

    Created initial functioning unit test for createPatientItem prior to refacotring

--- test/femr/common/ItemModelMapperTest.java
+++ test/femr/common/ItemModelMapperTest.java
@@ -1,8 +1,13 @@
 package femr.common;
 
 import femr.common.models.CityItem;
+import femr.common.models.PatientItem;
 import femr.utd.tests.BaseTest;
+import femr.util.calculations.dateUtils;
 import org.junit.*;
+
+import java.util.Calendar;
+import java.util.Date;
 
 import static org.junit.Assert.*;
 
@@ -15,6 +20,8 @@ public class ItemModelMapperTest extends BaseTest {
     private static String CITY_NAME = "Test City Name";
     private static String COUNTRY_NAME = "USA";
 
+    private static PatientItem expectedPatientItem;
+
     private static ItemModelMapper imm = new ItemModelMapper();
 
     @BeforeClass
@@ -22,7 +29,33 @@ public class ItemModelMapperTest extends BaseTest {
         expectedCityItem = new CityItem();
         expectedCityItem.setCityName(CITY_NAME);
         expectedCityItem.setCountryName(COUNTRY_NAME);
+    Calendar now = Calendar.getInstance();
+        now.add(Calendar.MONTH, -4);
+        Date birth = new Date();
+        birth.setTime(now.getTimeInMillis());
+
+        expectedPatientItem = new PatientItem();
+        expectedPatientItem.setAddress("1234 Main Street");
+        expectedPatientItem.setAge("4 MO");
+        expectedPatientItem.setBirth(birth);
+        expectedPatientItem.setCity("City");
+        expectedPatientItem.setFirstName("FName");
+        expectedPatientItem.setFriendlyDateOfBirth(dateUtils.getFriendlyDate(birth));
+        expectedPatientItem.setHeightFeet(6);
+        expectedPatientItem.setHeightInches(6);
+        expectedPatientItem.setId(1);
+        expectedPatientItem.setLastName("LName");
+        expectedPatientItem.setMonthsOld(4);
+        expectedPatientItem.setPathToPhoto("/dev/null");
+        expectedPatientItem.setPhotoId(1);
+        expectedPatientItem.setSex("M");
+        expectedPatientItem.setUserId(1);
+        expectedPatientItem.setWeeksPregnant(0);
+        expectedPatientItem.setWeight(Float.valueOf(5));
+        expectedPatientItem.setYearsOld(0);
     }
+
+
 
     @AfterClass
     public static void tearDown() throws Exception {
@@ -36,6 +69,51 @@ public class ItemModelMapperTest extends BaseTest {
         assertEquals("City Name", expectedCityItem.getCityName(), actualCityItem.getCityName());
 
         assertEquals("Country Name", expectedCityItem.getCountryName(), actualCityItem.getCountryName());
+
+    }
+
+    @Test
+    public void createPatientItem() throws Exception {
+        // Verify nominal create patient item
+        PatientItem actualPatientItem = imm.createPatientItem(expectedPatientItem.getId(),
+                expectedPatientItem.getFirstName(),
+                expectedPatientItem.getLastName(),
+                expectedPatientItem.getCity(),
+                expectedPatientItem.getAddress(),
+                expectedPatientItem.getUserId(),
+                expectedPatientItem.getBirth(),
+                expectedPatientItem.getSex(),
+                expectedPatientItem.getWeeksPregnant(),
+                expectedPatientItem.getHeightFeet(),
+                expectedPatientItem.getHeightInches(),
+                expectedPatientItem.getWeight(),
+                expectedPatientItem.getPathToPhoto(),
+                expectedPatientItem.getPhotoId(),
+                expectedPatientItem.getAge());
+
+
+        assertEquals(expectedPatientItem.getAddress(),actualPatientItem.getAddress());
+        assertEquals(expectedPatientItem.getAge(),actualPatientItem.getAge());
+        assertEquals(expectedPatientItem.getBirth(),actualPatientItem.getBirth());
+        assertEquals(expectedPatientItem.getCity(),actualPatientItem.getCity());
+        assertEquals(expectedPatientItem.getFirstName(),actualPatientItem.getFirstName());
+        assertEquals(expectedPatientItem.getFriendlyDateOfBirth(),actualPatientItem.getFriendlyDateOfBirth());
+        assertEquals(expectedPatientItem.getHeightFeet(),actualPatientItem.getHeightFeet());
+        assertEquals(expectedPatientItem.getHeightFeetDual(),actualPatientItem.getHeightFeetDual());
+        assertEquals(expectedPatientItem.getHeightInches(),actualPatientItem.getHeightInches());
+        assertEquals(expectedPatientItem.getHeightInchesDual(),actualPatientItem.getHeightInchesDual());
+        assertEquals(expectedPatientItem.getId(),actualPatientItem.getId());
+        assertEquals(expectedPatientItem.getLastName(),actualPatientItem.getLastName());
+        assertEquals(expectedPatientItem.getMonthsOld(),actualPatientItem.getMonthsOld());
+        assertEquals(expectedPatientItem.getPathToPhoto(),actualPatientItem.getPathToPhoto());
+        assertEquals(expectedPatientItem.getPhotoId(),actualPatientItem.getPhotoId());
+        assertEquals(expectedPatientItem.getSex(),actualPatientItem.getSex());
+        assertEquals(expectedPatientItem.getUserId(),actualPatientItem.getUserId());
+        assertEquals(expectedPatientItem.getWeeksPregnant(),actualPatientItem.getWeeksPregnant());
+        assertEquals(expectedPatientItem.getWeight(),actualPatientItem.getWeight());
+        assertEquals(expectedPatientItem.getWeightDual(),actualPatientItem.getWeightDual());
+        assertEquals(expectedPatientItem.getYearsOld(),actualPatientItem.getYearsOld());
+
 
     }
 
